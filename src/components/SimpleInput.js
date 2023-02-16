b216ce207e235436be6eda171aafd3834de3743b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 19:51:58 +0300

    Set up two way binding for the input form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -5,11 +5,16 @@ const SimpleInput = (props) =>
 {
   const [enteredName, setEnteredName] = useState('')
 
+  const nameInputChangeHandler = (e) =>
+  {
+    setEnteredName(e.target.value)
+  }
+
   return (
     <form>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
