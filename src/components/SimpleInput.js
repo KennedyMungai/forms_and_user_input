a369328290311d07de71575bae547d708c142199
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 19:56:10 +0300

    Added a console log for the enteredName state and the value property of the input prop

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -13,13 +13,15 @@ const SimpleInput = (props) =>
   const submitHandler = (e) => 
   {
     e.preventDefault()
+
+    console.log(enteredName)
   }
 
   return (
     <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' value={enteredName} onChange={nameInputChangeHandler} />
+        <input type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
