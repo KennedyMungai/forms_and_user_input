88e2d02cb02c424169b1ac9c048e5f2a1dd6de64
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 20:10:03 +0300

    Implemented some null handling logic for the entered text

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -18,6 +18,11 @@ const SimpleInput = (props) =>
 
     // console.log(enteredName)
 
+    if (enteredName.trim().length === 0)
+    {
+      return
+    }
+
     const enteredValue = nameInputRef.current.value
 
     console.log(enteredValue)
