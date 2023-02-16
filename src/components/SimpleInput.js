f496d6d3844a325d1b19104627fab0f187b216c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 19:58:47 +0300

    Defined the nameInputRef an wired it up

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,8 @@ import { useRef, useState } from "react";
 
 const SimpleInput = (props) =>
 {
+  const nameInputRef = useRef()
+
   const [enteredName, setEnteredName] = useState('')
 
   const nameInputChangeHandler = (e) =>
@@ -21,7 +23,7 @@ const SimpleInput = (props) =>
     <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} />
+        <input type='text' id='name' onChange={nameInputChangeHandler} ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
