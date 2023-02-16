934145f12837d1ce15554a62033e4ba03f393318
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-16 22:06:24 +0300

    Added code for the inferred states

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -4,6 +4,9 @@ const useInput = (props) =>
 {
     const [enteredValue, setEnteredValue] = useState('');
     const [isTouched, setIsTouched] = useState(false);
+
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 }
 
 export default useInput
